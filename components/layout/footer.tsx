1a468702587f6f74754a4d8085dccb2322bab7c0
aaaroz <ram.ardiansyah18@gmail.com>
2024-01-03 17:55:16 +0700

    refactor (footer) : change social media links

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -22,9 +22,7 @@ export const Footer: FC = () => {
           <div className="flex flex-col sm:flex-row sm:gap-8 h-5 mt-3 pb-32 sm:pb-0">
             <Accordion type="single" collapsible>
               <AccordionItem value="top-list" className="border-none">
-                <AccordionTrigger className="py-0 border-0">
-                  Top Lists
-                </AccordionTrigger>
+                <AccordionTrigger className="py-0 border-0">Top Lists</AccordionTrigger>
                 <AccordionContent>
                   <ul className="flex flex-col gap-1">
                     <Link href="/anime/top" className="hover:underline">
@@ -37,29 +35,23 @@ export const Footer: FC = () => {
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
-            <Separator
-              orientation="vertical"
-              className="hidden sm:block dark:bg-neutral-400"
-            />
+            <Separator orientation="vertical" className="hidden sm:block dark:bg-neutral-400" />
             <Link href="/anime" className="hover:underline">
               Anime
             </Link>
-            <Separator
-              orientation="vertical"
-              className="hidden sm:block dark:bg-neutral-400"
-            />
+            <Separator orientation="vertical" className="hidden sm:block dark:bg-neutral-400" />
             <Link href="/manga" className="hover:underline">
               Manga
             </Link>
           </div>
         </div>
         <div className="flex justify-center md:mr-5">
-          <Link href="/https://www.facebook.com/Rhamdanz18/" target="_blank">
+          <Link href="https://www.facebook.com/Rhamdanz18/" target="_blank">
             <div className="px-6 py-10 flex justify-center items-center hover:bg-gradient-to-b from-neutral-800 via-blue-900 dark:from-neutral-700 dark:via-blue-900 bg-opacity-70 w-full h-full">
               <FacebookIcon size={20} color="#ffffff" strokeWidth={1} />
             </div>
           </Link>
-          <Link href="/https://www.instagram.com/rmdnmar/" target="_blank">
+          <Link href="https://www.instagram.com/rmdnmar/" target="_blank">
             <div className="px-6 py-10 flex justify-center items-center hover:bg-gradient-to-b from-neutral-800 via-red-900 dark:from-neutral-700 dark:via-red-900 bg-opacity-70 w-full h-full">
               <InstagramIcon size={20} color="#ffffff" strokeWidth={1} />
             </div>
@@ -73,9 +65,7 @@ export const Footer: FC = () => {
       </div>
       <div className="text-neutral-400 bg-neutral-800 dark:bg-neutral-700 px-10">
         <Separator className="dark:bg-neutral-400" />
-        <p className="text-center text-xs py-3">
-          Copyright © 2023 ZORANIME, All rights reserved.
-        </p>
+        <p className="text-center text-xs py-3">Copyright © 2023 ZORANIME, All rights reserved.</p>
       </div>
     </div>
   );
