6123c01a2891bdf90e02104863a22e746c8adc18
aaaroz <ram.ardiansyah18@gmail.com>
2024-02-03 13:53:36 +0700

    refactor : add prisma client accelerate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,11 @@
-import { type Prisma, PrismaClient } from "@prisma/client";
-import { type DefaultArgs } from "@prisma/client/runtime/library";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-declare global {
-  var prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-let prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
